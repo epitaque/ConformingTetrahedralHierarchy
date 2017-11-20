e2ffa44e4737edf7666ac8e2cb7580611d44969d
Brian Hoy <brianhoy4@gmail.com>
2017-11-20 13:28:33 -0500

    upd

---
+++ Assets/DiamondMarchingCubes/LookupTableGenerator.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace DMC {
+public static class DMCLookupTableGenerator {
+    public static void Run() {
+
+    }
+
+    public static void CreateDiamondChildLookupTable(bool copy = false) {
+        // index: diamond's type
+        // diamond's type is  
+        Vector3[] childOffsets;
+
+
+
+    }
+
+    public static void GetDiamondType(Vector3Int centralVertex) {
+
+    }
+
+    public static int CountTrailingZeroes(int n) {
+        int count = 0;
+        for(int s = 0; s < 32; s++) {
+            if( (n >> s & 1) == 0) {
+                count++;
+            } 
+        }
+        return count;
+    }
+
+    public static GetType(float coordinate, int trailingZeroes) {
+
+        // 
+        int shifted = coordinate >>
+    }
+}
+}
